5d398d3f44da5d27f819b2e158e57401a3268b85
Heinrich Klobuczek <heinrich@mail.com>
2010-01-06 12:00:44 -0800

    fixed few minor things with shortest_path

--- lib/neo4jr-social/service.rb
+++ lib/neo4jr-social/service.rb
@@ -1,28 +1,35 @@
+java_import org.neo4j.graphalgo.shortestpath.Dijkstra
+java_import org.neo4j.graphalgo.shortestpath.std.IntegerEvaluator
+java_import org.neo4j.graphalgo.shortestpath.std.DoubleAdder
+java_import org.neo4j.graphalgo.shortestpath.std.DoubleComparator
+
 module Neo4jr
   class Service < Sinatra::Base
-            
+
     get '/info' do
       Neo4jr::DB.stats.to_json
     end
-    
+
     get '/nodes' do
       nodes = Neo4jr::DB.execute do |neo|
-        nodes = neo.all_nodes.map{|m| m.to_hash }        
+        nodes = neo.all_nodes.map{|m| m.to_hash }
       end
       nodes.to_json
     end
-  
+
     post '/nodes' do
       node = Neo4jr::DB.execute do |neo|
         node = neo.create_node(params)
       end
       node.to_hash.to_json
     end
-    
+
     get '/nodes/:node_id' do
-      Neo4jr::DB.getNodeById(params.delete('node_id')).to_json
+      node = Neo4jr::DB.execute do |neo|
+        neo.getNodeById(params.delete('node_id')).to_hash.to_json
+      end
     end
-      
+
     put '/nodes/:node_id' do
       node = Neo4jr::DB.execute do |neo|
         node = neo.getNodeById(params.delete('node_id'))
@@ -30,17 +37,17 @@ module Neo4jr
       end
       node.to_hash.to_json
     end
-    
+
     delete '/nodes/:node_id' do
       Neo4jr::DB.execute do |neo|
         node = neo.getNodeById(params['node_id'])
         node.delete
       end
     end
-    
+
     get '/nodes/:node_id/relationships' do
       relationships = Neo4jr::DB.execute do |neo|
-        node              = neo.getNodeById(params.delete('node_id'))
+        node = neo.getNodeById(params.delete('node_id'))
         if relationship_type = params.delete('type')
           relationship_type = RelationshipType.instance(relationship_type)
           node.getRelationships(relationship_type.to_a).hashify_objects
@@ -50,13 +57,13 @@ module Neo4jr
       end
       relationships.to_json
     end
-    
+
     post '/nodes/:node_id/relationships' do
       relationships = Neo4jr::DB.execute do |neo|
-        node              = neo.getNodeById(params.delete('node_id'))
-        to_node           = neo.getNodeById(params.delete('to'))
+        node = neo.getNodeById(params.delete('node_id'))
+        to_node = neo.getNodeById(params.delete('to'))
         relationship_type = RelationshipType.instance(params.delete('type'))
-        relationship      = node.create_relationship_to to_node, relationship_type
+        relationship = node.create_relationship_to to_node, relationship_type
         relationship.update_properties(params)
         node.getRelationships(relationship_type.to_a).hashify_objects
       end
@@ -65,44 +72,44 @@ module Neo4jr
 
     get '/nodes/:node_id/paths' do
       paths = Neo4jr::DB.execute do |neo|
-        start_node    = neo.getNodeById(params.delete('node_id'))
-        end_node      = neo.getNodeById(params.delete('to'))
-        relationship  = Neo4jr::RelationshipType.instance(params.delete('type'))
-        depth         = params.delete('depth') || 2
-        direction     = Neo4jr::Direction.from_string(params.delete('direction') || 'both')
+        start_node = neo.getNodeById(params.delete('node_id'))
+        end_node = neo.getNodeById(params.delete('to'))
+        relationship = Neo4jr::RelationshipType.instance(params.delete('type'))
+        depth = params.delete('depth') || 2
+        direction = Neo4jr::Direction.from_string(params.delete('direction') || 'both')
         shortest_path = AllSimplePaths.new(start_node, end_node, depth.to_i, direction, relationship.to_a)
         paths = shortest_path.getPaths
         paths.map{|p| p.map{|n| n.to_hash }}
       end
       paths.to_json
     end
-    
+
     get '/nodes/:node_id/shortest_path' do
       path = Neo4jr::DB.execute do |neo|
-        start_node    = neo.getNodeById(params.delete('node_id'))
-        end_node      = neo.getNodeById(params.delete('to'))
-        relationship  = Neo4jr::RelationshipType.instance(params.delete('type'))
-        dijkstra = Neo4jr::Dijkstra.new(
-          0.0,
-          start_node,
-          end_node,
-          Neo4jr::SimpleEvaluator.new,
-          DoubleAdder.new,
-          DoubleComparator.new,
-          Direction::BOTH,
-          relationship.to_a)
+        start_node = neo.getNodeById(params.delete('node_id'))
+        end_node = neo.getNodeById(params.delete('to'))
+        relationship = Neo4jr::RelationshipType.instance(params.delete('type'))
+        dijkstra = Dijkstra.new(
+                0.0,
+                start_node,
+                end_node,
+                Neo4jr::SimpleEvaluator.new,
+                DoubleAdder.new,
+                DoubleComparator.new,
+                Direction::BOTH,
+                relationship.to_a)
         dijkstra.getPath.map{|n| n.to_hash }
       end
       path.to_json
     end
-    
+
     get '/nodes/:node_id/recommendations' do
       suggestions = Neo4jr::DB.execute do |neo|
         relationship = Neo4jr::RelationshipType.incoming(params.delete('type'))
-        start_node   = neo.getNodeById(params.delete('node_id'))
-        level        = (params.delete('level') || 1).to_i
-        order        = Order::BREADTH_FIRST
-        return_when  = Return.when do |current_position|
+        start_node = neo.getNodeById(params.delete('node_id'))
+        level = (params.delete('level') || 1).to_i
+        order = Order::BREADTH_FIRST
+        return_when = Return.when do |current_position|
           current_position.depth > level
         end
         traverser = start_node.traverse(order, Stop.at(level + 1), return_when, relationship)
--- tmp/war/WEB-INF/lib/neo4jr-social/service.rb
+++ tmp/war/WEB-INF/lib/neo4jr-social/service.rb
@@ -1,28 +1,35 @@
+java_import org.neo4j.graphalgo.shortestpath.Dijkstra
+java_import org.neo4j.graphalgo.shortestpath.std.IntegerEvaluator
+java_import org.neo4j.graphalgo.shortestpath.std.DoubleAdder
+java_import org.neo4j.graphalgo.shortestpath.std.DoubleComparator
+
 module Neo4jr
   class Service < Sinatra::Base
-            
+
     get '/info' do
       Neo4jr::DB.stats.to_json
     end
-    
+
     get '/nodes' do
       nodes = Neo4jr::DB.execute do |neo|
-        nodes = neo.all_nodes.map{|m| m.to_hash }        
+        nodes = neo.all_nodes.map{|m| m.to_hash }
       end
       nodes.to_json
     end
-  
+
     post '/nodes' do
       node = Neo4jr::DB.execute do |neo|
         node = neo.create_node(params)
       end
       node.to_hash.to_json
     end
-    
+
     get '/nodes/:node_id' do
-      Neo4jr::DB.getNodeById(params.delete('node_id')).to_json
+      node = Neo4jr::DB.execute do |neo|
+        neo.getNodeById(params.delete('node_id')).to_hash.to_json
+      end
     end
-      
+
     put '/nodes/:node_id' do
       node = Neo4jr::DB.execute do |neo|
         node = neo.getNodeById(params.delete('node_id'))
@@ -30,17 +37,17 @@ module Neo4jr
       end
       node.to_hash.to_json
     end
-    
+
     delete '/nodes/:node_id' do
       Neo4jr::DB.execute do |neo|
         node = neo.getNodeById(params['node_id'])
         node.delete
       end
     end
-    
+
     get '/nodes/:node_id/relationships' do
       relationships = Neo4jr::DB.execute do |neo|
-        node              = neo.getNodeById(params.delete('node_id'))
+        node = neo.getNodeById(params.delete('node_id'))
         if relationship_type = params.delete('type')
           relationship_type = RelationshipType.instance(relationship_type)
           node.getRelationships(relationship_type.to_a).hashify_objects
@@ -50,42 +57,59 @@ module Neo4jr
       end
       relationships.to_json
     end
-    
+
     post '/nodes/:node_id/relationships' do
       relationships = Neo4jr::DB.execute do |neo|
-        node              = neo.getNodeById(params.delete('node_id'))
-        to_node           = neo.getNodeById(params.delete('to'))
+        node = neo.getNodeById(params.delete('node_id'))
+        to_node = neo.getNodeById(params.delete('to'))
         relationship_type = RelationshipType.instance(params.delete('type'))
-        relationship      = node.create_relationship_to to_node, relationship_type
+        relationship = node.create_relationship_to to_node, relationship_type
         relationship.update_properties(params)
         node.getRelationships(relationship_type.to_a).hashify_objects
       end
       relationships.to_json
     end
 
-    #optional direction & depth
-    get '/nodes/:node_id/path' do
+    get '/nodes/:node_id/paths' do
       paths = Neo4jr::DB.execute do |neo|
-        relationship  = Neo4jr::RelationshipType.instance(params.delete('type'))
-        start_node    = neo.getNodeById(params.delete('node_id'))
-        end_node      = neo.getNodeById(params.delete('to'))
-        depth         = params.delete('depth') || 2
-        direction     = Neo4jr::Direction.from_string(params.delete('direction') || 'both')
+        start_node = neo.getNodeById(params.delete('node_id'))
+        end_node = neo.getNodeById(params.delete('to'))
+        relationship = Neo4jr::RelationshipType.instance(params.delete('type'))
+        depth = params.delete('depth') || 2
+        direction = Neo4jr::Direction.from_string(params.delete('direction') || 'both')
         shortest_path = AllSimplePaths.new(start_node, end_node, depth.to_i, direction, relationship.to_a)
         paths = shortest_path.getPaths
         paths.map{|p| p.map{|n| n.to_hash }}
       end
       paths.to_json
     end
-    
-    #optional
+
+    get '/nodes/:node_id/shortest_path' do
+      path = Neo4jr::DB.execute do |neo|
+        start_node = neo.getNodeById(params.delete('node_id'))
+        end_node = neo.getNodeById(params.delete('to'))
+        relationship = Neo4jr::RelationshipType.instance(params.delete('type'))
+        dijkstra = Dijkstra.new(
+                0.0,
+                start_node,
+                end_node,
+                Neo4jr::SimpleEvaluator.new,
+                DoubleAdder.new,
+                DoubleComparator.new,
+                Direction::BOTH,
+                relationship.to_a)
+        dijkstra.getPath.map{|n| n.to_hash }
+      end
+      path.to_json
+    end
+
     get '/nodes/:node_id/recommendations' do
       suggestions = Neo4jr::DB.execute do |neo|
         relationship = Neo4jr::RelationshipType.incoming(params.delete('type'))
-        start_node   = neo.getNodeById(params.delete('node_id'))
-        level        = (params.delete('level') || 1).to_i
-        order        = Order::BREADTH_FIRST
-        return_when  = Return.when do |current_position|
+        start_node = neo.getNodeById(params.delete('node_id'))
+        level = (params.delete('level') || 1).to_i
+        order = Order::BREADTH_FIRST
+        return_when = Return.when do |current_position|
           current_position.depth > level
         end
         traverser = start_node.traverse(order, Stop.at(level + 1), return_when, relationship)
